3f7689eff32af2b23c284de16007d5e2fd017826
Razon Hossain <razon@razon.selise.com.bd>
2019-02-03 19:39:03 +0600

    first commit

---
+++ app/src/main/java/com/example/razon/wysiwyg/PreviewFragment.java
@@ -0,0 +1,109 @@
+package com.example.razon.wysiwyg;
+import android.graphics.Bitmap;
+import android.graphics.Typeface;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.text.Editable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+
+import com.github.irshulx.Editor;
+import com.github.irshulx.EditorListener;
+import com.github.irshulx.models.EditorContent;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class PreviewFragment extends Fragment {
+    private static final String SERIALIZED = "";
+
+    private String mSerialized;
+
+    private OnFragmentInteractionListener mListener;
+
+    public PreviewFragment() {
+    }
+    public static PreviewFragment newInstance(String serialized) {
+        PreviewFragment fragment = new PreviewFragment();
+        Bundle args = new Bundle();
+        args.putString(SERIALIZED, serialized);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mSerialized = getArguments().getString(SERIALIZED);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view= inflater.inflate(R.layout.fragment_preview, container, false);
+
+        Editor renderer= (Editor)view.findViewById(R.id.renderer);
+        Map<Integer, String> headingTypeface = getHeadingTypeface();
+        Map<Integer, String> contentTypeface = getContentface();
+        renderer.setHeadingTypeface(headingTypeface);
+        renderer.setContentTypeface(contentTypeface);
+        renderer.setDividerLayout(R.layout.tmpl_divider_layout);
+        renderer.setEditorImageLayout(R.layout.tmpl_image_view);
+        renderer.setListItemLayout(R.layout.tmpl_list_item);
+        String content= mSerialized;
+        EditorContent Deserialized= renderer.getContentDeserialized(content);
+        renderer.setEditorListener(new EditorListener() {
+            @Override
+            public void onTextChanged(EditText editText, Editable text) {
+
+            }
+
+            @Override
+            public void onUpload(Bitmap image, String uuid) {
+
+            }
+
+            @Override
+            public View onRenderMacro(String name, Map<String, Object> settings, int index) {
+                View view = getLayoutInflater().inflate(R.layout.layout_authored_by, null);
+                return view;
+            }
+        });
+        renderer.render(Deserialized);
+        return  view;
+    }
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
+    }
+
+    public Map<Integer,String> getHeadingTypeface() {
+        Map<Integer, String> typefaceMap = new HashMap<>();
+        typefaceMap.put(Typeface.NORMAL, "fonts/GreycliffCF-Bold.ttf");
+        typefaceMap.put(Typeface.BOLD, "fonts/GreycliffCF-Heavy.ttf");
+        typefaceMap.put(Typeface.ITALIC, "fonts/GreycliffCF-Heavy.ttf");
+        typefaceMap.put(Typeface.BOLD_ITALIC, "fonts/GreycliffCF-Bold.ttf");
+        return typefaceMap;
+    }
+
+    public Map<Integer,String> getContentface() {
+        Map<Integer, String> typefaceMap = new HashMap<>();
+        typefaceMap.put(Typeface.NORMAL,"fonts/Lato-Medium.ttf");
+        typefaceMap.put(Typeface.BOLD,"fonts/Lato-Bold.ttf");
+        typefaceMap.put(Typeface.ITALIC,"fonts/Lato-MediumItalic.ttf");
+        typefaceMap.put(Typeface.BOLD_ITALIC,"fonts/Lato-BoldItalic.ttf");
+        return typefaceMap;
+    }
+}
---
+++ app/src/main/java/com/example/razon/wysiwyg/RenderTestActivity.java
@@ -0,0 +1,29 @@
+package com.example.razon.wysiwyg;
+
+import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import com.example.razon.wysiwyg.Utilities.RendererPagerAdapter;
+
+
+public class RenderTestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_render_test);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        String serialized= getIntent().getStringExtra("content");
+        ViewPager viewPager = findViewById(R.id.viewpager);
+        viewPager.setAdapter(new RendererPagerAdapter(getSupportFragmentManager(),
+                RenderTestActivity.this,serialized));
+
+        // Give the TabLayout the ViewPager
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.sliding_tabs);
+        tabLayout.setupWithViewPager(viewPager);
+    }
+}
